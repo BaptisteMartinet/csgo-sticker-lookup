a7b357377df5f32306888e352797f9d77521aa67
Baptiste Martinet <baptiste.martinet@epitech.eu>
2021-09-19 17:55:28 +0200

    feat: implement parseMarketPages function

--- lookup.py
+++ lookup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-def main():
-  print("Script Running!")
+import requests
+import time
 
-if __name__ == "__main__":
+def parseMarketPages(nbPages):
+  for i in range(1, (nbPages + 1)):
+    res = requests.get('https://steamcommunity.com/market/search?appid=730#p{}_price_desc'.format(i))
+    print(res.status_code, res.url)
+    time.sleep(1)
+
+def main():
+  print('Script Running!')
+  parseMarketPages(3)
+
+if __name__ == '__main__':
   main()
